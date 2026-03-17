c6fa30135e459f11a63b52e98e85975e95afccd1
erikb <erikovnakara@yandex.ru>
2026-03-17 11:03:58 +0300

    add lint, noEmit

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+export default tseslint.config(
+  {
+    ignores: ["build/", "node_modules/", "dist/", "vite.config.ts"],
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
-  tseslint.configs.recommended,
-]);
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+);
